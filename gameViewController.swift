8a4e0d400fa0307c0587c1bb0cd8e56dd940764a
Paul Danielsons <danielpc@mail.uc.edu>
2019-04-24 13:27:21 -0400

    1.2
    Has limited gameplay functionality.

---
+++ gameViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GameViewController.swift
+//  WordMe
+//
+//  Created by Paul Danielsons on 12/12/18.
+//  Copyright © 2018 Paul Danielsons. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
